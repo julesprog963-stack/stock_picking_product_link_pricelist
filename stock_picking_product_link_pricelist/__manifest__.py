61aa5fd98d81c3c186b47db50e7299aa19c94ba8
julesprog963-stack <julesprog963@gmail.com>
2026-01-13 13:44:19 -0600

    Add HTML description and update author/website

--- stock_picking_product_link_pricelist/__manifest__.py
+++ stock_picking_product_link_pricelist/__manifest__.py
@@ -3,7 +3,8 @@
     "version": "17.0.1.0.1",
     "summary": "Show cost, list price, and pricelist price in received products list",
     "images": ["static/description/icon.png"],
-    "author": "Local",
+    "author": "JDA SOLUTIONS",
+    "website": "https://github.com/julesprog963-stack/stock_picking_product_link_pricelist",
     "license": "AGPL-3",
     "depends": ["stock_picking_product_link_port_v17", "product"],
     "data": [
