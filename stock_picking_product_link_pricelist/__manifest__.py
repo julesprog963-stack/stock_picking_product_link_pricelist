df7480024d9c233e906d311d8b9f4990758c5cfd
julesprog963-stack <julesprog963@gmail.com>
2026-01-13 13:41:24 -0600

    Set license to AGPL-3 for dependency compatibility

--- stock_picking_product_link_pricelist/__manifest__.py
+++ stock_picking_product_link_pricelist/__manifest__.py
@@ -4,7 +4,7 @@
     "summary": "Show cost, list price, and pricelist price in received products list",
     "images": ["static/description/icon.png"],
     "author": "Local",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "depends": ["stock_picking_product_link_port_v17", "product"],
     "data": [
         "views/product_tree_inherit.xml",
